8a2d4a336f70b33eaf379ea2a0a13da70ebb1568
Adam Hitchcock <adam@northisup.com>
2019-06-03 15:42:27 -0700

    dry it up

--- Casks/gitx-gitx.rb
+++ Casks/gitx-gitx.rb
@@ -3,20 +3,19 @@ cask 'gitx-gitx' do
   if MacOS.version <= :snow_leopard
     version '0.14.81'
     sha256 'ba61b4b84cb613a6196e6bd1d3102ad460ec0645a885b1cb94132e5244e1d330'
-    url "https://github.com/gitx/gitx/releases/download/builds/#{version.major_minor}/#{version.patch}/GitX-dev-#{version.patch}.dmg"
   elsif MacOS.version <= :lion
     version '0.15.1949'
     sha256 '17301ee1209bd4b12c126d7a58405efdb366f99f70b4f923a9c96e16c334ce2a'
-    url "https://github.com/gitx/gitx/releases/download/builds/#{version.major_minor}/#{version.patch}/GitX-dev-#{version.patch}.dmg"
   elsif MacOS.version <= :mavericks
     version '0.15.1964'
     sha256 'd88bcb7f92ca1cdf31cb3f1d2e24c03e2091ab330319aeef2e770c0dbd6f7817'
-    url "https://github.com/gitx/gitx/releases/download/builds/#{version.major_minor}/#{version.patch}/GitX-dev-#{version.patch}.dmg"
   else
     version '0.16.2327'
     sha256 'c8dd6dcac8dd85808acdb937f3125bf67b2b1c2b36da5541f20de73628abf544'
-    url "https://github.com/gitx/gitx/releases/download/build/#{version.major_minor}/#{version.patch}/GitX-dev-#{version.patch}.dmg"
+    s='s'
   end
+
+  url "https://github.com/gitx/gitx/releases/download/build#{s}/#{version.major_minor}/#{version.patch}/GitX-dev-#{version.patch}.dmg"
 
   appcast 'https://github.com/gitx/gitx/releases.atom'
   name 'GitX-dev'
